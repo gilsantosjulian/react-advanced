5f4007a95908ad0677341f13fc9c684ab89f6263
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-03-04 23:43:46 -0500

    feat: add webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
