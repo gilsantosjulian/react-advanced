c8fbdecd4b9f76c8dae4bd4491ee035cb31d2371
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-03-16 23:38:26 -0500

    feat: add categories from db.json

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Category } from '../Category'
+
 import { List, Item } from './styles' 
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
 
   return (
     <List>
       {
-        [1, 2, 3, 4].map((category) => 
-          <Item><Category /></Item>
+        categories.map((category) => 
+          <Item key={category.id}>
+            <Category {...category}/>
+          </Item>
         )
       }
     </List>
