0329db4e23c14026631b60b94ab1c507744c709f
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-03-18 08:21:33 -0500

    feat: add Logo and GlobalStyles into App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
-import { GlobalStyle } from './theme/globalStyles'
+import { Logo } from './components/Logo'
+import { GlobalStyle } from './styles/globalStyles'
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
