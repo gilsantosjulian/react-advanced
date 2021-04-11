d73122bc7e05af394ddaf1f47c029c8c2baec3cc
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-04-10 22:39:43 -0500

    feat:add new styles

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,23 +3,32 @@ import { Category } from '../Category'
 
 import { List, Item } from './styles' 
 
-export const ListOfCategories = async () => {
+export const ListOfCategories = () => {
 
   const [categories, setCategories ] = useState([])
+  const [showFixed, setShowFixed ] = useState(false)
   const url = 'http://localhost:3500/categories'
 
   useEffect(() => {
-    fetch(url)
+    window.fetch(url)
       .then(response => response.json())
-      .then(response => {
-        setCategories(response)
-      })
+      .then(response => setCategories(response))
   }, [])
   // [] -> Execute just when component is mounted
   // ['dependency'] -> Execute just when dependency changes
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return() => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => 
+    <List className={fixed ? 'fixed' : '' }>
       {
         categories.map((category) => 
           <Item key={category.id}>
@@ -28,5 +37,11 @@ export const ListOfCategories = async () => {
         )
       }
     </List>
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
