f41f717704a05b221c5cbfba7dd5717401fee074
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-04-15 20:15:03 -0500

    feat: add intersectionObserver and useRef

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
-import { ImgWrapper, Img, Button } from './styles'
+import { Article, ImgWrapper, Img, Button } from './styles'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
 
-      <Button>
-        <MdFavoriteBorder
-          size='32px'
-        /> 
-        {likes} likes!
-      </Button>
-    </article>
+  const element = useRef(null) // propiedad especial de React, que nos permite capturar el elemento del DOM
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    // console.log(element.current);
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting  } = entries[0]
+      if (isIntersecting) {
+        console.log('si');
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  return (
+    <Article ref={element}>
+      {
+        show && 
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder
+              size='32px'
+            /> 
+            {likes} likes!
+          </Button>
+        </>
+      }
+    </Article>
   )
 }
