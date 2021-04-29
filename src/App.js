be6c342dc81646bff9c6bb2cf59c11e80208e2de
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-04-29 00:11:01 -0500

    feat: add PhotoCardWithQuery reder prop container to app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/globalStyles'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 
 export const App = () => {
+  const qs = window.location.search
+  const urlParams = new window.URLSearchParams(qs)
+  const detailId = urlParams.get('detail')
+
   return (
     <>
       <GlobalStyle />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={2} />
+      {
+        detailId
+        ? <PhotoCardWithQuery id={detailId}/>  
+        :
+          <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+          </>
+      }
     </>
   )
 }
