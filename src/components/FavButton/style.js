e384925c4c1abd8284067520366c1f4726311272
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-04-30 20:15:29 -0500

    feat: create button component

---
+++ src/components/FavButton/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  display: flex;
+  padding-top: 8px;
+  align-items: center;
+  cursor: pointer;
+  & svg {
+    margin-right: 4px;
+  }
+`
