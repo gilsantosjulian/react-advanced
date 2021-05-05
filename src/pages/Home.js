142af2f22e5b755f4d3a7d396285887ba209619e
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-04 21:18:10 -0500

    feat: add id to get list of photos by category

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../components/ListOfPhotoCards'
 
-export const Home = () => {
+export const Home = ({ id }) => {
   return(
     <>
       <ListOfCategories />
-      <ListOfPhotoCards categoryId={2} />
+      <ListOfPhotoCards categoryId={id} />
     </>
   )
 }
