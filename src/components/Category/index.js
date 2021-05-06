4a22ec6e075185b71d4d4e810d7a2ad2ba8f6b28
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-05 22:47:19 -0500

    feat: add router link to Category component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Anchor, Image } from './styles'
+import { Link, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
 export const Category = ({
   cover = DEFAULT_IMAGE,
-  path,
+  path = '#',
   emoji = '?',
 }) => {
 
   return (
-    <Anchor href={path}>
+    <Link to={path}>
       <Image src={cover}/>
       {emoji}
-    </Anchor>
+    </Link>
   )
 
 }
