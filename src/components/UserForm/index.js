453e2dee0fdf5fa550ebf2eb6309bfe64c322b95
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-14 16:38:12 -0500

    feat: add styles for the user form

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { useInputValue } from '../../hooks/useInputValue';
+import { Button, Input, Form, Title } from './styles';
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
   return (
-    <form onSubmit={onSubmit}> 
-      <input placeholder='Email' {...email} />
-      <input placeholder='Password' {...password} />
-      <button>Iniciar sesión</button>
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}> 
+        <Input placeholder='Email' {...email} />
+        <Input placeholder='Password' {...password} />
+        <Button>{title}</Button>
+      </Form>
+    </>
   );
 }
