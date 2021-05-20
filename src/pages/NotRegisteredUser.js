59995b001e4abad8c751750a906a270d75404f9c
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-19 22:50:30 -0500

    feat: call mutation on not registered user page

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { useStateValue } from '../state';
 import { UserForm } from '../components/UserForm';
+import { useMutationRegister } from '../hooks/useRegisterMutation';
 
 export const NotRegisteredUser = () => {
   const [{ isAuth }, dispatch] = useStateValue()
+  const { mutation, mutationLoading, mutationError } = useMutationRegister()
   const dispatchOpts = {
     type: 'authenticate',
     isAuth: true,
   }
 
-  const handleOnSubmit = () => dispatch(dispatchOpts)
+  // const handleOnSubmit = () => dispatch(dispatchOpts)
 
+  const handleOnSubmit = ({ email, password }) => {
+    const input = { email, password }
+    console.log({input});
+    mutation({
+      variables: { input }
+    })
+    .then(res => {
+      dispatch(dispatchOpts)
+    })
+  }
   return (
-    <UserForm onSubmit={handleOnSubmit} />
+    <>
+      <UserForm onSubmit={handleOnSubmit} title='Registrarse' />
+      <UserForm onSubmit={handleOnSubmit} title='Iniciar sesión' />
+    </>
   );
 }
