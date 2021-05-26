f13f82486cbcfb110f611b20e4074e5a94e7add6
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-25 22:19:53 -0500

    feat: sessionStorage set and get methods

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,11 @@
 export const initialState = {
-  isAuth: false,
+  isAuth: window.sessionStorage.getItem('token')
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'authenticate':
+      action.token && window.sessionStorage.setItem('token', action.token)
       return {
         ...state,
         isAuth: action.isAuth
