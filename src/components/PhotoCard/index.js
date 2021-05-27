94c06ebcbe888f4a8d39066799d6f9d1a6dc10c9
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-26 21:16:16 -0500

    refactor: remove code and get props from server

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,19 +8,16 @@ import { useMutationToogleLike } from '../../hooks/useMutationToogleLike'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `liked-${id}`
-  const [liked, setLike] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const { mutation, mutationLoading, mutationError } = useMutationToogleLike()
 
   const handleFavClick = () => {
-    !liked && mutation({
+    mutation({
       variables: {
         input: { id }
       }
     })
-    setLike(!liked)
   }
 
   return (
